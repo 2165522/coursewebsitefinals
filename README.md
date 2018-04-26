9ca745088081db4c2d77411b7a28771b504abed4
2165522 <2165522@slu.edu.ph>
2018-04-27 03:00:01 +0800

    Applied php scripts in the website

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # WebTek Lec
-<h2>Frameworks</h2>
-<ul>
-<li>
-    <a href="http://materializecss.com/">Materializecss</a>
-</li>
-</ul>
+<html>
+
+<body>
+    <h2>HOW TO ACCESS WEBSITE IN WAMPSERVER</h2>
+    <ol>
+        <li>Clone repository in your computer.</li>
+        <strong>NOTE: You only clone once.</strong>
+        <li>Copy the whole repository (copy the folder, not the contents) in wampserver www directory ( C:/wamp64/www/ )</li>
+        <li>Start wampserver, make sure that All services is running.</li>
+        <li>Access website in your browser with the url <i>localhost/coursewebsitefinals</i></li>
+    </ol>
+    <h2>REMINDERS</h2>
+    <ul>
+        <li>Your repository is still active even when it is hosted in wamp.</li>
+        <li>When changes are committed by other members, DON'T CLONE THE REPOSITORY AGAIN. Just right click in the window of your repository and open Git then enter the command <b>git pull origin</b> to update your repository.</li>
+    </ul>
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,175 @@
+<?php
+    header("Content-Type: text/html");
+    include("pagefragments/header.html");
+?>
+
+<!-- Server-Side Web Scripting Technologies section-->
+
+<div id="index-banner" class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+        <div class="container">
+
+            <h1 class="header right white-text">Server-side Web Scripting Technologies</h1>
+
+            <br><br>
+
+        </div>
+    </div>
+    <div class="parallax"><img src="./img/background1.jpg" alt="Unsplashed background img 1"></div>
+</div>
+
+<!--   Servlet Section   -->
+<div class="container">
+    <div class="section">
+
+
+
+        <div class="row">
+
+            <div class="col s12 m6">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">subject</i></h2>
+                    <h5 class="center">Review Course Notes</h5>
+
+                    <p class="center light">Take time in reviewing java servlets.</p>
+
+                    <!-- Link of review-->
+
+                    <ul class="center">
+                        <li><a href="./links/Servlets.html" class="btn-large waves-effect waves-light #80cbc4 teal lighten-3">Review</a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <!-- Take the quiz -->
+
+            <div class="col s12 m6">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+                    <h5 class="center">Take the quiz</h5>
+
+                    <p class="center light">Test your knowledge about java servlets.</p>
+
+                    <!-- Take the quiz link -->                                                        
+                    <ul class="center">
+                        <li><a href="#" class="btn-large waves-effect waves-light #80cbc4 teal lighten-3">Quiz</a></li>
+                    </ul>                        
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!-- PHP section-->
+
+
+<div id="index-banner" class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+        <div class="container">
+
+
+            <div class="row center">
+
+            </div>
+
+            <br><br>
+
+        </div>
+    </div>
+    <div class="parallax"><img src="./img/background2.jpg" alt="Unsplashed background img 1"></div>
+</div>
+
+<!--   Servlet Section   -->
+<div class="container">
+    <div class="section">
+
+
+
+        <div class="row">
+
+            <div class="col m12">
+                <h2 class="header center  blue-text">PHP</h2>
+                <h3 class="header center blue-text">(Hypertext Preprocessor)</h3>
+            </div>
+            <div class="col s12 m6">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">subject</i></h2>
+                    <h5 class="center">Review Course Notes</h5>
+
+                    <p class="center light">Take time in reviewing php scripts.</p>
+
+                    <!-- Link of review-->
+
+                    <ul class="center">
+                        <li><a href="./links/PHP.html" class="btn-large waves-effect waves-light #80cbc4 teal lighten-3">Review</a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <!-- Take the quiz -->
+
+            <div class="col s12 m6">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+                    <h5 class="center">Take the quiz</h5>
+
+                    <p class="center light">Test your knowledge about php scripting.</p>
+
+                    <!-- Take the quiz link -->                                                        
+                    <ul class="center">
+                        <li><a href="#" class="btn-large waves-effect waves-light #80cbc4 teal lighten-3">Quiz</a></li>
+                    </ul>                        
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>   
+
+
+<footer class="page-footer teal #f4511e deep-orange darken-1">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Company Bio</h5>
+                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Settings</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Connect</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+
+</body>
+
+</html>
