2ea9ec215d7012338137decc8a0bbaf5013b9d60
2166538 <2166538@slu.edu.ph>
2018-05-03 11:36:45 +0800

    edited signup.html

--- ScriptCademy_PHP/links/signup.php
+++ ScriptCademy_PHP/links/signup.php
@@ -1,13 +1,11 @@
 <?php
-
 header("HTTP/1.1 200 OK");
 header("Content-Type: text/html");
 include("../pagefragments/header.html");
-
 ?>
         <div class="container">
-            <div class="row">
-                <div class="col s12 m6 offset-m3">
+		<div class="row">
+				<div class="col s12 m6 offset-m8">
                     <div class="card white darken-1">
                         <div class="card-content blue-text">
                             <span class="card-title">Sign Up</span>
@@ -15,6 +13,16 @@ include("../pagefragments/header.html");
                             <div class="row">
                                 <form class="col s12">
                                     <div class="row">
+										<div class="input-field col s6 m6">
+                                            <i class="material-icons waves-light prefix">person</i>
+                                            <input id="icon_prefix" type="text" class="validate">
+                                            <label for="icon_prefix">First Name</label>
+                                        </div>
+										<div class="input-field col s6 m6">
+                                            <i class="material-icons waves-light prefix">perm_identity</i>
+                                            <input id="icon_prefix" type="text" class="validate">
+                                            <label for="icon_prefix">Last Name</label>
+                                        </div>
                                         <div class="input-field col s12 m12">
                                             <i class="material-icons waves-light prefix">account_circle</i>
                                             <input id="icon_prefix" type="text" class="validate">
@@ -34,16 +42,23 @@ include("../pagefragments/header.html");
                                             <i class="material-icons waves-light prefix">lock</i>
                                             <input id="icon_prefix" type="password" class="validate">
                                             <label for="icon_prefix">Confirm Password</label>
-                                        </div> 
-                                        <div class="col s12 m6 offset-m4">
-                                            <button class="btn waves-effect waves-light" type="submit" name="action">Sign me up!</button> 
                                         </div>
-                                        <div class="col s6 m6 offset-m3">     
-                                            <p> <br></p>
-                                        </div>  
-                                        <div class="col s12 m12 offset-m2">
-                                            <a href="#signup.html">Already have an account? Sign In now!</a>      
-                                        </div>  
+										<div class="col s12 m12">
+										 <form action="#">
+											<div class="file-field input-field col s12 m12">
+											  <div class="btn">
+												<span>File</span>
+												<input type="file" multiple>
+											  </div>
+											  <div class="file-path-wrapper col s12 m9">
+												<input class="file-path validate" type="text" placeholder="Upload profile image">
+											  </div>
+											</div>
+										 </form>
+										 </div>
+										<div class="col s12 m6 offset-m4">
+                                            <button class="btn waves-effect waves-light" type="submit" name="action">Sign me up!</button> 
+                                        </div>									
                                     </div>
                                 </form>
                             </div>        
@@ -51,6 +66,6 @@ include("../pagefragments/header.html");
                     </div>
                 </div>
             </div>
-        </div>
+		</div>
 <?php
 include("../pagefragments/footer.html");
