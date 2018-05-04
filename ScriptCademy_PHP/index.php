bbbf9717e8f9f3ed971f9ca4ae6f53177612eb53
2166538 <2166538@slu.edu.ph>
2018-05-04 10:50:19 +0800

    edited PHP

--- ScriptCademy_PHP/index.php
+++ ScriptCademy_PHP/index.php
@@ -2,8 +2,9 @@
 header("HTTP/1.1 200 OK");
 header("Content-Type: text/html");
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -14,10 +15,11 @@ header("Content-Type: text/html");
         <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection" />
         <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
         <style>
-        i.waves-light {
-        color: waves-light;
-        }
-        </style>       
+            i.waves-light {
+                color: waves-light;
+            }
+
+        </style>
     </head>
 
     <body>
@@ -49,17 +51,21 @@ header("Content-Type: text/html");
                         <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
                         <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
                     </ul>
-                <a id="logo-container" href="#" class="brand-logo light-blue-text text-darken-4"></a>
-                <ul class="right hide-on-med-and-down ">
-                    <li><a href="#">Java Servlets</a></li>
-                    <li><a href="#">PHP</a></li>
-                </ul>
+                    <a id="logo-container" href="#" class="brand-logo light-blue-text text-darken-4"></a>
+                    <ul class="right hide-on-med-and-down ">
+                        <li><a href="#">Java Servlets</a></li>
+                        <li><a href="#">PHP</a></li>
+                    </ul>
 
                 </ul>
             </div>
         </nav>
         <ul class="sidenav" id="mobile-demo">
-            <li class="light-blue-text text-darken-4"><div class="container"><h5>Course Notes</h5></div></li>
+            <li class="light-blue-text text-darken-4">
+                <div class="container">
+                    <h5>Course Notes</h5>
+                </div>
+            </li>
             <li><a href="!#" class="dropdown-trigger" data-target="servlets">Java Servlets</a></li>
             <li><a href="#">PHP</a></li>
             <li><a href="#">Javascript</a></li>
@@ -74,42 +80,42 @@ header("Content-Type: text/html");
                         <div class="col s12 m6">
                             <h1 class="header white-text">Server-side Web Scripting Technologies</h1>
                         </div>
-            <div class="col s12 m6">
-                <div class="card white darken-1">
-                    <div class="card-content blue-text">
-                    <span class="card-title">Sign In</span>
-                        <hr>
-                        <div class="row">
-                        <form class="col s12">
-                          <div class="row">
-                            <div class="input-field col s6 m12">
-                              <i class="material-icons waves-light prefix">account_circle</i>
-                              <input id="icon_prefix" type="text" class="validate">
-                              <label for="icon_prefix">Username</label>
+                        <div class="col s12 m4 offset-m2">
+                            <div class="card white darken-1">
+                                <div class="card-content blue-text">
+                                    <span class="card-title">Sign In</span>
+                                    <hr>
+                                    <div class="row">
+                                        <form class="col s12">
+                                            <div class="row">
+                                                <div class="input-field col s6 m12">
+                                                    <i class="material-icons waves-light prefix">account_circle</i>
+                                                    <input id="icon_prefix" type="text" class="validate">
+                                                    <label for="icon_prefix">Username</label>
+                                                </div>
+                                                <div class="input-field col s6 m12">
+                                                    <i class="material-icons waves-light prefix">security</i>
+                                                    <input id="icon_prefix" type="password" class="validate">
+                                                    <label for="icon_prefix">Password</label>
+                                                </div>
+                                                <div class="col s12 m6 offset-m4">
+                                                    <button class="btn waves-effect waves-light" type="submit" name="action">Sign In</button>
+                                                </div>
+                                                <div class="col s6 m6 offset-m3">
+                                                    <p> <br></p>
+                                                </div>
+                                                <div class="col s6 m6 offset-m3">
+                                                    <a href="links/signup.php">Create an account now!</a>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="input-field col s6 m12">
-                              <i class="material-icons waves-light prefix">security</i>
-                              <input id="icon_prefix" type="password" class="validate">
-                              <label for="icon_prefix">Password</label>
-                            </div> 
-                            <div class="col s12 m6 offset-m4">
-                             <button class="btn waves-effect waves-light" type="submit" name="action">Sign In</button> 
-                            </div>
-                            <div class="col s6 m6 offset-m3">     
-                                <p> <br></p>
-                            </div>  
-                            <div class="col s6 m6 offset-m3">
-                             <a href="#signup.html">Create an account now!</a>      
-                            </div>  
-                          </div>
-                        </form>
-                    </div>        
+                        </div>
                     </div>
                 </div>
             </div>
-                            </div>
-            </div>
-        </div>
             <div class="parallax"><img src="img/background1.jpg" alt="Unsplashed background img 1"></div>
         </div>
 
@@ -146,10 +152,10 @@ header("Content-Type: text/html");
 
                             <p class="center light">Test your knowledge about java servlets.</p>
 
-                            <!-- Take the quiz link -->                                                        
+                            <!-- Take the quiz link -->
                             <ul class="center">
                                 <li><a href="#" class="btn-large waves-effect waves-light #80cbc4 teal lighten-3">Quiz</a></li>
-                            </ul>                        
+                            </ul>
                         </div>
                     </div>
 
@@ -161,22 +167,22 @@ header("Content-Type: text/html");
         <!-- PHP section-->
 
 
- <div id="index-banner" class="parallax-container valign-wrapper">
+        <div id="index-banner" class="parallax-container valign-wrapper">
             <div class="section no-pad-bot">
                 <div class="container">
                     <div class="row">
                         <div class="col s12 m6 l6">
                             <h1 class="header white-text">PHP (Hypertext Preprocessor)</h1>
-                         
+
                         </div>
-                        
+
                     </div>
 
                 </div>
             </div>
             <div class="parallax"><img src="./img/background2.jpg" alt="Unsplashed background img 1"></div>
         </div>
-        
+
 
         <!--   PHP Section   -->
         <div class="container">
@@ -211,17 +217,17 @@ header("Content-Type: text/html");
 
                             <p class="center light">Test your knowledge about php scripting.</p>
 
-                            <!-- Take the quiz link -->                                                        
+                            <!-- Take the quiz link -->
                             <ul class="center">
                                 <li><a href="#" class="btn-large waves-effect waves-light #80cbc4 teal lighten-3">Quiz</a></li>
-                            </ul>                        
+                            </ul>
                         </div>
                     </div>
 
                 </div>
 
             </div>
-        </div>  
+        </div>
 
         <footer class="page-footer light-blue darken-4">
             <div class="container">
@@ -250,24 +256,21 @@ header("Content-Type: text/html");
                     </div>
                 </div>
             </div>
-
         </footer>
-
 
         <!--  Scripts-->
         <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="js/bin/materialize.min.js"></script>
         <script src="js/bin/init.js"></script>
         <script>
-            $(document).ready(function(){
+            $(document).ready(function() {
                 $('.dropdown-trigger').dropdown();
             });
-            $(document).ready(function(){
+            $(document).ready(function() {
                 $('.collapsible').collapsible();
             });
+
         </script>
     </body>
 
-</html>
-
-
+    </html>
